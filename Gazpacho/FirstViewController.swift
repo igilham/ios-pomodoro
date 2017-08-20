0bd4dd36038e427944a17bc3da2b60787500626a
Ian Gilham <ian.gilham@bbc.co.uk>
2017-08-20 12:16:46 +0100

    initial project creation

---
+++ Gazpacho/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gazpacho
+//
+//  Created by Ian Gilham on 20/08/2017.
+//  Copyright © 2017 Ian Gilham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Gazpacho/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gazpacho
+//
+//  Created by Ian Gilham on 20/08/2017.
+//  Copyright © 2017 Ian Gilham. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
